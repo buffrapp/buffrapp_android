c7f127fa6026070b2b6989e17fc2c111af0d26c8
FacuM <facumo.fm@gmail.com>
2019-11-15 02:07:08 -0300

    Begin blank implementation and skeleton of Schedule activity.

---
+++ app/src/main/java/com/buffrapp/Schedule.java
@@ -0,0 +1,157 @@
+package com.buffrapp;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class Schedule extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private EditText etReportContent;
+    private AlertDialog dialog;
+
+    private RelativeLayout rlReportAlert;
+
+    private DrawerHandler drawerHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+
+        drawerHandler = new DrawerHandler(this);
+
+        etReportContent = new EditText(this);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_schedule);
+        navigationView.bringToFront();
+
+        drawer.addDrawerListener(drawerHandler);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_report:
+                rlReportAlert = new RelativeLayout(Schedule.this);
+                RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT
+                );
+                rlReportAlert.setPadding(getResources().getDimensionPixelSize(R.dimen.activity_horizontal_margin),
+                        0,
+                        getResources().getDimensionPixelSize(R.dimen.activity_horizontal_margin),
+                        0);
+
+                TextWatcher afterTextChangedListener = new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        String reportContent = etReportContent.getText().toString();
+
+                        if (reportContent.length() < 1) {
+                            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+                            etReportContent.setError(getString(R.string.report_invalid_content));
+                        } else {
+                            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(true);
+                        }
+                    }
+                };
+
+                etReportContent.setHeight(RelativeLayout.LayoutParams.MATCH_PARENT);
+                etReportContent.setHint(getString(R.string.report_hint));
+                etReportContent.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+                etReportContent.setGravity(Gravity.TOP);
+                etReportContent.addTextChangedListener(afterTextChangedListener);
+
+                rlReportAlert.addView(etReportContent, layoutParams);
+
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(Schedule.this)
+                        .setTitle(getString(R.string.report_title))
+                        .setView(rlReportAlert)
+                        .setPositiveButton(getString(R.string.action_send), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ReportWorker reportWorker = new ReportWorker(Schedule.this);
+                                reportWorker.setReportContent(etReportContent.getText().toString());
+                                reportWorker.execute();
+                            }
+                        })
+                        .setNegativeButton(getString(R.string.action_cancel), null)
+                        .setOnDismissListener(new DialogInterface.OnDismissListener() {
+                            @Override
+                            public void onDismiss(DialogInterface dialog) {
+                                rlReportAlert.removeAllViews();
+                            }
+                        });
+
+                dialog = dialogBuilder.create();
+                dialog.show();
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+
+                break;
+            default:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        drawerHandler.setNavCurrentId(item.getItemId());
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
